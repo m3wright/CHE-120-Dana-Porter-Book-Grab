4b0819aadba2ed8582476425e241d763ff212441
Marcus <m3wright@uwaterloo.ca>
2024-11-17 16:34:12 -0500

    Add files via upload

---
+++ movementWgrid.py
@@ -0,0 +1,330 @@
+import pygame , sys
+from pygame import *
+from sys import exit
+from random import randint
+
+ground_y=0
+ground_x=0
+pygame.init()
+screen = pygame.display.set_mode((1200,800))
+clock = pygame.time.Clock()
+tile_size = 90
+player_pos = 1
+
+ground = pygame.image.load("Floor Design 10 - 6.png").convert_alpha()
+ground=pygame.transform.scale(ground, (52*tile_size,53*tile_size))
+ground_rect = ground.get_rect(topleft = (-7*tile_size,-18*tile_size))
+
+table_img = pygame.image.load('Table top.png')
+grass_img = pygame.image.load('Bookmid Horizontal.png')
+
+br = pygame.image.load('Bookend Right.png').convert_alpha()
+bl =pygame.image.load('Bookend Left.png').convert_alpha()
+bu = pygame.image.load('Bookend up.png').convert_alpha()
+bd =pygame.image.load('Bookend Down.png').convert_alpha()
+bv =pygame.image.load('Bookmid vertical.png').convert_alpha()
+bh =pygame.image.load('Bookmid Horizontal.png').convert_alpha()
+
+player_forward = pygame.image.load("Move Forward.png").convert_alpha()
+player_forward = pygame.transform.scale(player_forward, (80,80))
+player_rect = player_forward.get_rect(center= (600,400))
+player_forward_rect = player_forward.get_rect(center= (600,400))
+
+player_back = pygame.image.load("Move Back.png").convert_alpha()
+player_back = pygame.transform.scale(player_back, (80,80))
+player_back_rect = player_back.get_rect(center= (600,400))
+
+player_left = pygame.image.load("Move Left.png").convert_alpha()
+player_left = pygame.transform.scale(player_left, (80,80))
+player_left_rect = player_left.get_rect(center= (600,400))
+
+player_right = pygame.image.load("Move Right.png").convert_alpha()
+player_right = pygame.transform.scale(player_right, (80,80))
+player_right_rect = player_right.get_rect(center= (600,400))
+
+chair = pygame.image.load("chair.png").convert_alpha()
+chair_up = pygame.image.load("chair up.png").convert_alpha()
+chair_left = pygame.image.load("chair left.png").convert_alpha()
+chair_right = pygame.image.load("chair right.png").convert_alpha()
+iwall = pygame.image.load("iwall.png").convert_alpha()
+owall = pygame.image.load("owall.png").convert_alpha()
+table = pygame.image.load("table.png").convert_alpha()
+
+moveSpeed = 10
+
+world_data = [
+[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+[1, 0, 0, 0, 0,10,14,14,11, 0,10,14,14,11, 0,10,14,14,11, 0,10,14,14,11, 0,10,14,14,11, 0,10,14,14,11, 0,10,14,14,11, 0,10,14,14,11, 0,10,14,14,11, 0, 0, 0, 0, 1],
+[1, 0, 0, 0, 0, 0,14,14, 0, 0, 0,14,14, 0, 0, 0,14,14, 0, 0, 0,14,14, 0, 0, 0,14,14, 0, 0, 0,14,14, 0, 0, 0,14,14, 0, 0, 0,14,14, 0, 0, 0,14,14, 0, 0, 0, 0, 0, 1],
+[1, 8, 0, 8, 0,10,14,14,11, 0,10,14,14,11, 0,10,14,14,11, 0,10,14,14,11, 0,10,14,14,11, 0,10,14,14,11, 0,10,14,14,11, 0,10,14,14,11, 0,10,14,14,11, 0, 8, 0, 8, 1],
+[1,14,14,14, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,14,14,14, 1],
+[1,14,14,14, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,14,14,14, 1],
+[1, 9, 0, 9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9, 0, 9, 1],
+[1, 0, 0, 0, 0, 0, 0, 3, 7, 7, 7, 7, 7, 7, 7, 7, 7, 2, 0, 0,12,12,12,12,12,12,12,12,12,12,12,12,12,12, 0, 0, 3, 7, 7, 7, 7, 7, 7, 7, 7, 7, 2, 0, 0, 0, 0, 0, 0, 1],
+[1, 8, 0, 8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,12,13,13,13,13,13,13,13,13,13,13,13,13,12, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8, 0, 8, 1],
+[1,14,14,14, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,12,13,13,13,13,13,13,13,13,13,13,13,13,12, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,14,14,14, 1],
+[1,14,14,14, 0, 0, 0, 3, 7, 7, 7, 7, 7, 7, 7, 7, 7, 2, 0, 0,12,13,13,13,13,13,13,13,13,13,13,13,13,12, 0, 0, 3, 7, 7, 7, 7, 7, 7, 7, 7, 7, 2, 0, 0, 0,14,14,14, 1],
+[1, 9, 0, 9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,12,13,13,13,13,13,13,13,13,13,13,13,13,12, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,12,13,13,13,13,13,13,13,13,13,13,13,13,12, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+[1, 8, 0, 8, 0, 0, 0, 3, 7, 7, 7, 7, 7, 7, 7, 7, 7, 2, 0, 0,12,13,13,13,13,13,13,13,13,13,13,13,13,12, 0, 0, 3, 7, 7, 7, 7, 7, 7, 7, 7, 7, 2, 0, 0, 0, 8, 0, 8, 1],
+[1,14,14,14, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,12,13,13,13,13,13,13,13,13,13,13,13,13,12, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,14,14,14, 1],
+[1,14,14,14, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,12,13,13,13,13,13,13,13,13,13,13,13,13,12, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,14,14,14, 1],
+[1, 9, 0, 9, 0, 0, 0, 3, 7, 7, 7, 7, 7, 7, 7, 7, 7, 2, 0, 0,12,13,13,13,13,13,13,13,13,13,13,13,13,12, 0, 0, 3, 7, 7, 7, 7, 7, 7, 7, 7, 7, 2, 0, 0, 0, 9, 0, 9, 1],
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,12,13,13,13,13,13,13,13,13,13,13,13,13,12, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+[1, 8, 0, 8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,12,12,12,12,12,12,13,13,13,13,13,13,13,12, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8, 0, 8, 1],
+[1,14,14,14, 0, 0, 0, 3, 7, 7, 7, 7, 7, 7, 7, 7, 7, 2, 0, 0, 0, 0, 0, 0, 0,12,12,12,12,12,12,12,12,12, 0, 0, 3, 7, 7, 7, 7, 7, 7, 7, 7, 7, 2, 0, 0, 0,14,14,14, 1],
+[1,14,14,14, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,14,14,14, 1],
+[1, 9, 0, 9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,12,12,12,12,12,12,12,12,12, 0, 0, 0, 9, 0, 9, 1],
+[1, 0, 0, 0, 0, 0, 0,12,12,12,12,12,12,12, 0, 0, 0, 0, 0, 0, 0, 3, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 2, 0, 0, 0, 0,12,13,13,13,13,13,13,13,12, 0, 0, 0, 0, 0, 0, 1],
+[1, 8, 0, 8, 0, 0, 0,12,13,13,13,13,13,12, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,12,13,13,13,13,13,13,13,12, 0, 0, 0, 8, 0, 8, 1],
+[1,14,14,14, 0, 0, 0,12,13,13,13,13,13,12,12,12,12,12, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,12,13,13,13,13,13,13,13,12, 0, 0, 0,14,14,14, 1],
+[1,14,14,14, 0, 0, 0,12,13,13,13,13,13,13,13,13,13,12, 0, 0, 0, 3, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 2, 0, 0, 0, 0,12,13,13,13,13,13,13,13,12, 0, 0, 0,14,14,14, 1],
+[1, 9, 0, 9, 0, 0, 0,12,13,13,13,13,13,13,13,13,13,12, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,12,12,12,13,13,13,13,13,13,13,12, 0, 0, 0, 9, 0, 9, 1],
+[1, 0, 0, 0, 0, 0, 0,12,13,13,13,13,13,13,13,13,13,12, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,12,13,13,13,13,13,13,13,13,13,12, 0, 0, 0, 0, 0, 0, 1],
+[1, 8, 0, 8, 0, 0, 0,12,13,13,13,13,13,13,13,13,13,12, 0, 0, 0, 3, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 2, 0, 0,12,13,13,13,13,13,13,13,13,13,12, 0, 0, 0, 8, 0, 8, 1],
+[1,14,14,14, 0, 0, 0,12,13,13,13,13,13,13,13,13,13,12, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,12,13,13,13,13,13,13,13,13,13,12, 0, 0, 0,14,14,14, 1],
+[1,14,14,14, 0, 0, 0,12,13,13,13,13,13,13,13,13,13,12, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,12,13,13,13,13,13,13,13,13,13,12, 0, 0, 0,14,14,14, 1],
+[1, 9, 0, 9, 0, 0, 0,12,12,12,12,12,12,12,12,12,12,12, 0, 0, 0, 3, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 2, 0, 0,12,12,12,12,12,12,12,12,12,12,12, 0, 0, 0, 9, 0, 9, 1],
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+[1, 8, 0, 8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8, 0, 8, 1],
+[1,14,14,14, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,14,14,14, 1],
+[1,14,14,14, 0, 0, 0, 3, 7, 7, 7, 7, 7, 7, 7, 7, 7, 2, 0, 0, 0, 3, 7, 7, 7, 7, 7, 7, 7, 7, 7, 2, 0, 0, 0, 0, 3, 7, 7, 7, 7, 7, 7, 7, 7, 7, 2, 0, 0, 0,14,14,14, 1],
+[1, 9, 0, 9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+[1, 8, 0, 8, 0, 0, 0, 3, 7, 7, 7, 7, 7, 7, 7, 7, 7, 2, 0, 0, 0, 3, 7, 7, 7, 7, 7, 7, 7, 7, 7, 2, 0, 0, 0, 0, 3, 7, 7, 7, 7, 7, 7, 7, 7, 7, 2, 0, 0, 0, 8, 0, 8, 1],
+[1,14,14,14, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,14,14,14, 1],
+[1,14,14,14, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,14,14,14, 1],
+[1, 9, 0, 9, 0, 0, 0, 3, 7, 7, 7, 7, 7, 7, 7, 7, 7, 2, 0, 0, 0, 3, 7, 7, 7, 7, 7, 7, 7, 7, 7, 2, 0, 0, 0, 0, 3, 7, 7, 7, 7, 7, 7, 7, 7, 7, 2, 0, 0, 0, 9, 0, 9, 1],
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+[1, 8, 0, 8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8, 0, 8, 1],
+[1,14,14,14, 0, 0, 0, 3, 7, 7, 7, 7, 7, 7, 7, 7, 7, 2, 0, 0, 0, 3, 7, 7, 7, 7, 7, 7, 7, 7, 7, 2, 0, 0, 0, 0, 3, 7, 7, 7, 7, 7, 7, 7, 7, 7, 2, 0, 0, 0,14,14,14, 1],
+[1,14,14,14, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,14,14,14, 1],
+[1, 9, 0, 9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9, 0, 9, 1],
+[1, 0, 0, 0, 0, 0, 0, 3, 7, 7, 7, 7, 7, 7, 7, 7, 7, 2, 0, 0, 0, 3, 7, 7, 7, 7, 7, 7, 7, 7, 7, 2, 0, 0, 0, 0, 3, 7, 7, 7, 7, 7, 7, 7, 7, 7, 2, 0, 0, 0, 0, 0, 0, 1],
+[1, 8, 0, 8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8, 0, 8, 1],
+[1,14,14,14, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,14,14,14, 1],
+[1,14,14,14, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,14,14,14, 1],
+[1, 9, 0, 9, 0,10,14,14,11, 0,10,14,14,11, 0,10,14,14,11, 0,10,14,14,11, 0,10,14,14,11, 0,10,14,14,11, 0,10,14,14,11, 0,10,14,14,11, 0,10,14,14,11, 0, 9, 0, 9, 1],
+[1, 0, 0, 0, 0, 0,14,14, 0, 0, 0,14,14, 0, 0, 0,14,14, 0, 0, 0,14,14, 0, 0, 0,14,14, 0, 0, 0,14,14, 0, 0, 0,14,14, 0, 0, 0,14,14, 0, 0, 0,14,14, 0, 0, 0, 0, 0, 1],
+[1, 0, 0, 0, 0,10,14,14,11, 0,10,14,14,11, 0,10,14,14,11, 0,10,14,14,11, 0,10,14,14,11, 0,10,14,14,11, 0,10,14,14,11, 0,10,14,14,11, 0,10,14,14,11, 0, 0, 0, 0, 1],
+[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+
+]
+
+tile_list = []
+
+row_count =-19
+for row in world_data:
+    col_count = -7 
+    for tile in row:
+        if tile == 1:
+            img = pygame.transform.scale(table_img, (tile_size, tile_size))
+            img_rect = img.get_rect(topright = (col_count*tile_size,row_count*tile_size))
+            tile = (img, img_rect)
+            tile_list.append(tile)
+        elif tile == 2:
+            img = pygame.transform.scale(br, (tile_size, tile_size))
+            img_rect = img.get_rect(topright = (col_count*tile_size,row_count*tile_size))
+            tile = (img, img_rect)
+            tile_list.append(tile)
+            
+        elif tile == 3:
+            img = pygame.transform.scale(bl, (tile_size, tile_size))
+            img_rect = img.get_rect(topright = (col_count*tile_size,row_count*tile_size))
+            tile = (img, img_rect)
+            tile_list.append(tile)
+        
+        elif tile == 4:
+            img = pygame.transform.scale(bu, (tile_size, tile_size))
+            img_rect = img.get_rect(topright = (col_count*tile_size,row_count*tile_size))
+            tile = (img, img_rect)
+            tile_list.append(tile)
+                
+        elif tile == 5:
+            img = pygame.transform.scale(bd, (tile_size, tile_size))
+            img_rect = img.get_rect(topright = (col_count*tile_size,row_count*tile_size))
+            tile = (img, img_rect)
+            tile_list.append(tile)
+            
+        elif tile == 6:
+            img = pygame.transform.scale(bv, (tile_size, tile_size))
+            img_rect = img.get_rect(topright = (col_count*tile_size,row_count*tile_size))
+            tile = (img, img_rect)
+            tile_list.append(tile)
+        
+        elif tile == 7:
+            img = pygame.transform.scale(bh, (tile_size, tile_size))
+            img_rect = img.get_rect(topright = (col_count*tile_size,row_count*tile_size))
+            tile = (img, img_rect)
+            tile_list.append(tile)
+            
+        elif tile == 8:
+            img = pygame.transform.scale(chair, (tile_size, tile_size))
+            img_rect = img.get_rect(topright = (col_count*tile_size,row_count*tile_size))
+            tile = (img, img_rect)
+            tile_list.append(tile)
+        
+        elif tile == 9:
+            img = pygame.transform.scale(chair_up, (tile_size, tile_size))
+            img_rect = img.get_rect(topright = (col_count*tile_size,row_count*tile_size))
+            tile = (img, img_rect)
+            tile_list.append(tile)
+        
+        elif tile == 10:
+            img = pygame.transform.scale(chair_left, (tile_size, tile_size))
+            img_rect = img.get_rect(topright = (col_count*tile_size,row_count*tile_size))
+            tile = (img, img_rect)
+            tile_list.append(tile)
+        
+        elif tile == 11:
+            img = pygame.transform.scale(chair_right, (tile_size, tile_size))
+            img_rect = img.get_rect(topright = (col_count*tile_size,row_count*tile_size))
+            tile = (img, img_rect)
+            tile_list.append(tile)
+        
+        elif tile == 12:
+            img = pygame.transform.scale(owall, (tile_size, tile_size))
+            img_rect = img.get_rect(topright = (col_count*tile_size,row_count*tile_size))
+            tile = (img, img_rect)
+            tile_list.append(tile)
+            
+        elif tile == 13:
+            img = pygame.transform.scale(iwall, (tile_size, tile_size))
+            img_rect = img.get_rect(topright = (col_count*tile_size,row_count*tile_size))
+            tile = (img, img_rect)
+            tile_list.append(tile)
+        
+        elif tile == 14:
+            img = pygame.transform.scale(table, (tile_size, tile_size))
+            img_rect = img.get_rect(topright = (col_count*tile_size,row_count*tile_size))
+            tile = (img, img_rect)
+            tile_list.append(tile)
+    
+        col_count += 1
+    row_count += 1
+
+    
+
+while True:
+    screen.fill("Grey")
+    
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_a: 
+                moveLeft = True
+                ground_x=moveSpeed
+            elif event.key == pygame.K_d:
+                moveRight = True
+                ground_x=-moveSpeed
+            elif event.key == pygame.K_w: 
+                moveUp=True
+                ground_y=moveSpeed
+            elif event.key == pygame.K_s: 
+                moveDown=True
+                ground_y=-moveSpeed
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_a: 
+                moveLeft = True
+                ground_x=0
+            elif event.key == pygame.K_d:
+                moveRight = True
+                ground_x=0
+            elif event.key == pygame.K_w: 
+                moveUp=True
+                ground_y=0
+            elif event.key == pygame.K_s: 
+                moveDown=True
+                ground_y=0
+    
+    ground_rect.y += ground_y
+    ground_rect.x += ground_x      
+
+          
+    
+    
+    for tile in tile_list:
+        tile[1].x += ground_x
+        tile[1].y += ground_y
+        
+        pygame.draw.rect(screen, (255, 255, 255), tile[1], 2)
+    
+    for tile in tile_list:
+        #check for collision in x direction
+        if player_rect.colliderect(tile[1]):
+            
+            if ground_x < 0:
+                ground_rect.x += moveSpeed
+            
+            elif ground_x > 0:
+                ground_rect.x -= moveSpeed
+            
+            if ground_y < 0:
+                ground_rect.y += moveSpeed
+            
+            elif ground_y > 0:
+                ground_rect.y -= moveSpeed
+            
+            
+            for tile in tile_list:
+                if ground_x < 0:
+                    tile[1].x += moveSpeed
+                
+                elif ground_x > 0:
+                    tile[1].x -= moveSpeed
+                
+                if ground_y < 0:
+                    tile[1].y += moveSpeed
+                
+                elif ground_y > 0:
+                    tile[1].y -= moveSpeed
+                    
+                
+                screen.blit(tile[0], tile[1])
+                pygame.draw.rect(screen, (255, 255, 255), tile[1], 2)
+    
+    screen.blit(ground,ground_rect)
+    
+    for tile in tile_list:
+        screen.blit(tile[0],tile[1])
+    
+    if ground_x < 0:
+        screen.blit(player_right,player_left_rect)
+        player_pos = 1
+    
+    elif ground_x > 0:
+        screen.blit(player_left,player_right_rect)
+        player_pos = 2
+        
+    elif ground_y < 0:
+        screen.blit(player_back,player_back_rect) 
+        player_pos = 3
+    
+    elif ground_y > 0:
+        screen.blit(player_forward,player_forward_rect)
+        player_pos = 4
+    
+    else:
+        if player_pos == 1:
+            screen.blit(player_right,player_right_rect)
+        
+        elif player_pos == 2:
+            screen.blit(player_left,player_left_rect)
+        
+        elif player_pos == 3:
+            screen.blit(player_back,player_back_rect)
+        
+        elif player_pos == 4:
+            screen.blit(player_forward,player_forward_rect)
+        
+    
+    #row_count = 0
+    
+
+    #ground_y=0
+    #ground_x=0
+    
+    pygame.display.update()
+    clock.tick(60)
